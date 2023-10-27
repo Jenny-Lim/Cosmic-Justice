4175bacaab724d784a9b1bc338474a29fefc92f6
Jenny Lim <180jnlm@gmail.com>
2023-10-27 16:28:47 -0400

    comment

--- Cosmic-Justice/Assets/Scripts/UI/SliderObject.cs
+++ Cosmic-Justice/Assets/Scripts/UI/SliderObject.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using Unity.VisualScripting;
 
-public class SliderObject : MonoBehaviour
+public class SliderObject : MonoBehaviour // deprecated
 {
     private Slider slider;
     private RectTransform sliderRT;
