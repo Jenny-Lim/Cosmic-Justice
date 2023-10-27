f4f847e87b216ec2cb4e0493ec219c3d64fac7d6
Jenny Lim <180jnlm@gmail.com>
2023-10-27 17:05:46 -0400

    randomly making safe zone

--- Cosmic-Justice/Assets/Scripts/DialMinigame/Dial.cs
+++ Cosmic-Justice/Assets/Scripts/DialMinigame/Dial.cs
@@ -13,12 +13,12 @@ public class Dial : MonoBehaviour
     [SerializeField] Slider health;
     [SerializeField] GameObject safeAreaStart, safeAreaEnd;
 
-    [SerializeField] float decrSpeed;
-    [SerializeField] float incrAmt;
-    [SerializeField] float decrHealth;
-    [SerializeField] float incrHealth;
+    [SerializeField] float decrSpeed, incrAmt;
+    [SerializeField] float decrHealth, incrHealth;
+    [SerializeField] float minRand, maxRand;
 
     private float currAngle, rangeStart, rangeEnd; // angles work in negatives where you think the value would be positive, and vice versa
+    private float randTime;
 
     void Start()
     {
@@ -33,16 +33,30 @@ public class Dial : MonoBehaviour
 
         safeAreaStart.transform.rotation = Quaternion.Euler(0f, 0f, rangeStart);
         safeAreaEnd.transform.rotation = Quaternion.Euler(0f, 0f, rangeEnd);
+
+        randTime = Random.Range(minRand, maxRand);
     }
 
     void Update()
     {
         Debug.Log("currAngle: " + currAngle);
         needle.transform.rotation = Quaternion.Euler(0f, 0f, currAngle);
+
+        safeAreaStart.transform.rotation = Quaternion.Euler(0f, 0f, rangeStart);
+        safeAreaEnd.transform.rotation = Quaternion.Euler(0f, 0f, rangeEnd);
     }
 
     void FixedUpdate()
     {
+        randTime -= Time.deltaTime;
+        if (randTime <= 0)
+        { // get random vals again
+            rangeStart = Random.Range(-1, -170);
+            rangeEnd = Random.Range(rangeStart, -180);
+
+            randTime = Random.Range(minRand, maxRand);
+        }
+
         // loss
         if (currAngle >= 0 || health.value <= 0)
         {
