e36089bb66ad21fcd56617c629c25cf799212e0b
Jenny Lim <180jnlm@gmail.com>
2023-10-27 17:15:09 -0400

    start on verdict

---
+++ Cosmic-Justice/Assets/Scripts/VerdictButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerdictButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // animate it coming up
+        // make interactable
+        // some animation on press / function / text -- inheritance ??? switches ??
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
