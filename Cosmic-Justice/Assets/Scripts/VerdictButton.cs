53e11c75205491f53d47f8d47918b934a056a7ec
Jenny-Lim <180jnlm@gmail.com>
2023-10-27 21:56:11 -0400

    Update VerdictButton.cs

--- Cosmic-Justice/Assets/Scripts/VerdictButton.cs
+++ Cosmic-Justice/Assets/Scripts/VerdictButton.cs
@@ -25,7 +25,7 @@ public class VerdictButton : MonoBehaviour
 
     public void DoneComeUp() // animation event
     {
-        b.interactable = true; // object ref error
+        b.interactable = true;
         Debug.Log("done come up!");
     }
 }
