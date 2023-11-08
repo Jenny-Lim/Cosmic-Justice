13f9525c29843141fb9ede76cb83bdd50b8939e1
Jenny-Lim <180jnlm@gmail.com>
2023-11-07 23:07:30 -0500

    cleaning

--- Cosmic-Justice/Assets/Scripts/DialMinigame/Dial.cs
+++ Cosmic-Justice/Assets/Scripts/DialMinigame/Dial.cs
@@ -6,6 +6,7 @@ using TMPro;
 
 public class Dial : MonoBehaviour
 {
+    // objects
     [SerializeField] GameObject needle;
     [SerializeField] TMP_Text statusText;
     [SerializeField] Button button;
@@ -13,78 +14,50 @@ public class Dial : MonoBehaviour
     [SerializeField] public Slider health;
     [SerializeField] GameObject safeArea;
 
+    // variables
     [SerializeField] float decrSpeed, incrAmt;
-    [SerializeField] float decrHealth;//, incrHealth;
+    [SerializeField] float decrHealth;
     [SerializeField] float minRand, maxRand;
     [SerializeField] float lerpSpeed;
     [SerializeField] float rangeSize;
 
-    private float currAngle, rangeStart;//, rangeEnd; // angles work in negatives where you think the value would be positive, and vice versa
+    private float currAngle, rangeStart; // angles work in negatives where you think the value would be positive, and vice versa
     private float randTime, t;
 
     void Start() // use width to alter range, make range check a collider enter, keep range the same
     {
-        //gameIsOver = false; // have a default safe area
         t = 0f;
 
         health.interactable = false;
         health.maxValue = 50;
         health.value = health.maxValue;
 
-        currAngle = -0;
+        currAngle = 0;
 
         rangeStart = Random.Range(-90, 90);
-        //rangeEnd = Random.Range(rangeStart + 1, -180);
 
         safeArea.transform.rotation = Quaternion.Euler(0f, 0f, rangeStart);
-        //safeAreaEnd.transform.rotation = Quaternion.Euler(0f, 0f, rangeEnd);
 
         randTime = Random.Range(minRand, maxRand);
-    }
+    } // Start
 
     void Update()
     {
-        //Debug.Log("currAngle: " + currAngle);
         needle.transform.rotation = Quaternion.Euler(0f, 0f, currAngle);
-
-            //safeAreaStart.transform.rotation = Quaternion.Euler(0f, 0f, rangeStart);
-            safeArea.transform.rotation = Quaternion.Lerp(safeArea.transform.rotation, Quaternion.Euler(0f, 0f, rangeStart+rangeSize), t * lerpSpeed);
-
-            //safeAreaEnd.transform.rotation = Quaternion.Euler(0f, 0f, rangeEnd);
-            //safeAreaEnd.transform.rotation = Quaternion.Lerp(safeAreaEnd.transform.rotation, Quaternion.Euler(0f, 0f, rangeEnd), t * lerpSpeed);
-
-        t += Time.deltaTime; // in fixed update pls
-    }
+        safeArea.transform.rotation = Quaternion.Lerp(safeArea.transform.rotation, Quaternion.Euler(0f, 0f, rangeStart + rangeSize), t * lerpSpeed);
+    } // Update
 
     void FixedUpdate()
     {
+        t += Time.deltaTime; // in fixed update pls
+
         randTime -= Time.deltaTime;
-        //t += Time.deltaTime;
-
-        //if (!gameIsOver)
-        //{ // get random vals again
-            if (randTime <= 0) // move into else
-            {
-                //rangeStart = Mathf.Lerp(rangeStart, Random.Range(0, -170), t * lerpSpeed);
-                //rangeEnd = Mathf.Lerp(rangeEnd, Random.Range(rangeStart + 1, -180), t * lerpSpeed);
-
-                rangeStart = Random.Range(-90, 90);
-                //rangeEnd = Random.Range(rangeStart + 1, -180);
-
-                randTime = Random.Range(minRand, maxRand);
-            }
-
-            //safeAreaStart.transform.rotation = Quaternion.Lerp(safeAreaStart.transform.rotation, Quaternion.Euler(0f, 0f, rangeStart), t * lerpSpeed);
-            //safeAreaEnd.transform.rotation = Quaternion.Lerp(safeAreaEnd.transform.rotation, Quaternion.Euler(0f, 0f, rangeEnd), t * lerpSpeed);
-        //}
-
 
         // loss
         if (health.value <= 0)
         {
-            statusText.text = "You lose."; // use a bool for this
+            statusText.text = "You lose.";
             button.enabled = false;
-            //gameIsOver = true;
             // make time stop
             // exit here
         }
@@ -94,33 +67,38 @@ public class Dial : MonoBehaviour
         {
             statusText.text = "You Win!";
             button.enabled = false;
-            //gameIsOver = true;
             // make time stop
             // exit here
         }
 
         else
         {
+            if (randTime <= 0)
+            {
+                rangeStart = Random.Range(-90, 90);
+                randTime = Random.Range(minRand, maxRand);
+            }
+
+            health.value -= decrHealth * Time.deltaTime;
+
             if (currAngle < 0)
             {
                 currAngle += decrSpeed * Time.deltaTime;
             }
 
-            //// for health
-            //if (currAngle <= rangeStart && currAngle >= rangeEnd) // could instead do this based on rotation
-            //{
-            //    statusText.text = "Phew";
-            //    health.value += incrHealth * Time.deltaTime;
-            //}
-            //else
-            //{
-            //    statusText.text = "Welp";
-                health.value -= decrHealth * Time.deltaTime;
-            //}
+            if (health.value >= health.maxValue)
+            {
+                statusText.text = "Phew";
+            }
+            else
+            {
+                statusText.text = "Welp";
+                
+            }
 
         }
 
-    }
+    } // FixedUpdate
 
     public void IncrementDial()
     {
--- Cosmic-Justice/Assets/Scripts/DialMinigame/SafeArea.cs
+++ Cosmic-Justice/Assets/Scripts/DialMinigame/SafeArea.cs
@@ -6,24 +6,10 @@ using UnityEngine;
 public class SafeArea : MonoBehaviour
 {
     [SerializeField] Dial dial;
-    [SerializeField] TMP_Text statusText;
     [SerializeField] float incrHealth;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerStay2D()
     {
-        //statusText.text = "Phew";
         dial.health.value += incrHealth * Time.deltaTime;
-        Debug.Log(dial.health.value);
-    }
+    } // OnTriggerStay2D
 }
