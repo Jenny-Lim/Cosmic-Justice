80b78aa7c4d4e9abd4a6fc4cf6e3e078d6eb0655
Jenny Lim <180jnlm@gmail.com>
2024-01-12 14:27:25 -0500

    ajhsKD

---
+++ Cosmic-Justice/Assets/Scripts/DeskObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskObject : MonoBehaviour
+{
+    void OnEnable()
+    {
+        // lerp it up
+    }
+
+    public void BringDown()
+    {
+        // lerp it down
+    }
+}
