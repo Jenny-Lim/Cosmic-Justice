699506ffa7ca13b0b490a01dad23c856b541457c
Jenny-Lim <180jnlm@gmail.com>
2024-01-17 02:22:49 -0500

    tutorial ui start

---
+++ Cosmic-Justice/Assets/Scripts/Flash.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flash : MonoBehaviour
+{
+    private Image image;
+    [SerializeField] private CanvasRenderer text;
+
+    void OnEnable()
+    {
+        image = GetComponent<Image>();
+        StartCoroutine("FlashCORO");
+    }
+
+    IEnumerator FlashCORO()
+    {
+        while (true) {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
+            text.cull = false;
+            yield return new WaitForSeconds(1f);
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+            text.cull = true;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
