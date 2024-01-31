dba63bf47444c6d716c8eb5e5f14aabaa27e85e9
AASUN <71030185+A-A-RONSUN@users.noreply.github.com>
2024-01-30 20:33:35 -0500

    Add new input system, case 3 debug, virtual mouse
    Added the Unity new input system for when we use controller. Added case 3 debug key to get to start of the dialogue. Also began working on the virtual mouse needed for the controller controlling.

---
+++ Cosmic-Justice/Assets/Scripts/VirtualMouse.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualMouse : MonoBehaviour
+{
+
+    public static VirtualMouse instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        Cursor.visible = false;
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
