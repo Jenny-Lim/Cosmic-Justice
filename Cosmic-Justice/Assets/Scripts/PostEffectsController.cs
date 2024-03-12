4858738cc5676b4d021d3c72a342e0f34ae2e48e
Jenny-Lim <180jnlm@gmail.com>
2024-03-12 18:50:12 -0400

    color cont

--- Cosmic-Justice/Assets/Scripts/PostEffectsController.cs
+++ Cosmic-Justice/Assets/Scripts/PostEffectsController.cs
@@ -8,11 +8,15 @@ public class PostEffectsController : MonoBehaviour
     public Shader shader;
     Material mat;
 
+    public Color color;
+
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
         if (mat == null) mat = new Material(shader);
 
         RenderTexture renderTex = RenderTexture.GetTemporary(src.width, src.height, 0, src.format);
+
+        mat.SetColor("_color", color);
 
         // alter
         Graphics.Blit(src, renderTex, mat); // apply shader
