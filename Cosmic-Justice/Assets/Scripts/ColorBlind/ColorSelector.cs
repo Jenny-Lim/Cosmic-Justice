b7fa0d3f8ffd0030a54afa5abe4d483cdd1af760
Jenny Lim <180jnlm@gmail.com>
2024-03-19 20:07:15 -0400

    Update ColorSelector.cs

--- Cosmic-Justice/Assets/Scripts/ColorBlind/ColorSelector.cs
+++ Cosmic-Justice/Assets/Scripts/ColorBlind/ColorSelector.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.PlayerSettings;
 
 public class ColorSelector : MonoBehaviour
 {
@@ -26,7 +27,18 @@ public class ColorSelector : MonoBehaviour
     public void OnColorPick()
     {
         // onmouseclick, save color, change color of button to that color, and hide the color wheel image
-        selectedColor = colorWheel.GetPixel( (int)( Input.mousePosition.x ), (int)( Input.mousePosition.y ));
+        //selectedColor = colorWheel.GetPixel( (int)( Input.mousePosition.x ), (int)( Input.mousePosition.y ));
+
+        RaycastHit hit;
+        Camera _cam = Camera.main; // Camera to use for raycasting
+        Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(_cam.transform.position, ray.direction, out hit, 10000.0f);
+
+        if (hit.collider)
+        {
+            selectedColor = colorWheel.GetPixel((int)hit.textureCoord2.x, (int)hit.textureCoord2.y); // Get color from texture
+        }
+
         thisButton.color = selectedColor;
         colorWheelObj.SetActive(false);
     }
